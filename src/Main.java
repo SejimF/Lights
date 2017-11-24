910bca888f1c9a3c2e9d150c2548a9b52e9e9f10
sejimf <kolodkomaxim@gmail.com>
2017-11-24 18:44:51 +0300

    2

--- src/Main.java
+++ src/Main.java
@@ -24,14 +24,35 @@ public class Main
 		Timer secondsTimer = new Timer();
 		Timer secSecondTimer = new Timer();
 		
+		
+		for(int i = 0; i < 19; i++){
+			try{
+				lconstr.ligths[i].changeLightState();
+				Date dateNow = new Date();
+				System.out.println( sf.format(dateNow) + " " + lconstr.ligths[i].getLightId() + " "  + lconstr.ligths[i].getLightColor() + " " +lconstr.ligths[i].isLState());
+				Thread.sleep(1000);
+				Date dateNowSecond = new Date();
+				lconstr.ligths[i].changeLightState();
+				System.out.println( sf.format(dateNowSecond) + " " + lconstr.ligths[i].getLightId() + " "  + lconstr.ligths[i].getLightColor() + " " +lconstr.ligths[i].isLState());
+				
+				
+			}catch(Exception e){
+				
+			}
+			}
+		
+		/**
+		
+		final int i = 0;
 		secondsTimer.scheduleAtFixedRate(new TimerTask(){
 			@Override
 			public void run(){
-				lconstr.ligths[5].changeLightState();
+				lconstr.ligths[i].changeLightState();
 				Date dateNow = new Date();
-				System.out.println( sf.format(dateNow) + " " + lconstr.ligths[5].getLightId() + " "  + lconstr.ligths[5].getLightColor() + " " +lconstr.ligths[5].isLState());
-				
-			}
+				System.out.println( sf.format(dateNow) + " " + lconstr.ligths[i].getLightId() + " "  + lconstr.ligths[i].getLightColor() + " " +lconstr.ligths[i].isLState());
+			
+				}
+	
 		}, 1000 , 2000);
 		
 		secSecondTimer.scheduleAtFixedRate(new TimerTask(){
@@ -44,7 +65,7 @@ public class Main
 					
 				}
 			}, 2000 , 2000);
-		
+		**/
 		
 	}
 }
